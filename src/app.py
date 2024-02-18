90f842c7e8ba803d83ff23fc5ae6a04907b96a82
E. Escalante <eduardo1582000@gmail.com>
2024-02-18 17:52:05 -0600

    Práctica 1 con Flask. Crear un sitio web en flask, siguiendo los pasos vistos en clases, que al ingresar muestre el mensaje "Bienvenido al sitio Web (su nombre).

--- src/app.py
+++ src/app.py
@@ -10,5 +10,22 @@ def index():
     }
     return render_template('index.html', data = datosWeb)
 
+# Ruta para mostrar un mensaje personalizado
+@app.route('/mensaje/<nombre>')
+def mensaje(nombre):
+    datosMensaje = {
+        "titulo":"Mensaje para "+nombre,
+        "nombre": nombre
+    }
+    return render_template('mensaje.html', data = datosMensaje)
+
+
+# Ruta que usa el formulario para crear el mensaje personalizado
+@app.route('/crearmensaje')
+def crearmensaje():
+    name = request.args['name'] # lo que se escribió en el nombre
+    return redirect(url_for('mensaje', nombre = name)) # redirigir url
+
+
 # Iniciar la applicación
 app.run(debug=True)
