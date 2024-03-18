2200f1752980daba8cc4adeeae0460329acddfb7
E. Escalante <eduardo1582000@gmail.com>
2024-03-17 20:23:28 -0600

    Navbar

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import config
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_USER'] = config.MYSQL_USER
 app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
 app.config['MYSQL_DB'] = config.MYSQL_DB
-app.config['HEX_SEC_KEY'] = config.HEX_SEC_KEY
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY
 
 mysql = MySQL(app)
 
@@ -20,15 +20,16 @@ def login():
     password = request.form['password']
     cur = mysql.connection.cursor()
     cur.execute('select * from users where email = %s and password = %s', (email, password))
-    info = cur.fetchone()
+    user = cur.fetchone()
     cur.close()
-
-    if info == None:
-        print('No se encontró en la base de datos')
-        return render_template('login.html', message = '¡Correo o contraseña incorrectos!')
-    else:
-        print(f'Sí se encontró en la base de datos: {info}')
+    if user is not None:
+        session['email'] = email
+        session['name'] = user[1]
+        session['surnames'] = user[2]
+        print(f'Sí se encontró en la base de datos: {user}')
         return redirect(url_for('success'))
+    else:
+        return render_template('login.html', message = '¡Correo o contraseña incorrectos!')
 
 @app.route('/success')
 def success():
